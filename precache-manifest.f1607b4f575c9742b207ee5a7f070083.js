0be745a2c45ba3910f185eb2b48a2f825852742a
Aman Gupta <50948920+justAmanGupta@users.noreply.github.com>
2020-04-18 14:59:06 +0530

    homepage added

---
+++ precache-manifest.f1607b4f575c9742b207ee5a7f070083.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c02b7f7bab34dbc09a8ec55d0e912ef",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7d2090630c3a33d08553",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ed01c54d79fb0b527e85",
+    "url": "/robofriends/static/css/main.1040dc9d.chunk.css"
+  },
+  {
+    "revision": "7d2090630c3a33d08553",
+    "url": "/robofriends/static/js/2.c548b687.chunk.js"
+  },
+  {
+    "revision": "ed01c54d79fb0b527e85",
+    "url": "/robofriends/static/js/main.16ade7f3.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
